1be1fa20f68f08847dddda27ce91c75308dda6a7
Kendall Arneaud <kendall.arneaud@gmail.com>
2023-06-24 09:55:14 +0000

    test that files created and remove test data

--- tests/GenerateTest.php
+++ tests/GenerateTest.php
@@ -11,7 +11,7 @@ class GenerateTest extends TestCase
     protected $generate;
 
     public function setUp():Void {
-        $this->generate = new Generate('App\Models', __DIR__ . '/Mocks/swagger.yaml', 'tests/data', false, null, null);
+        $this->generate = new Generate('App', __DIR__ . '/Mocks/swagger.yaml', 'tests/data', false, null, null);
         
     }
 
@@ -24,4 +24,34 @@ class GenerateTest extends TestCase
     {
         $this->assertGreaterThan(0, $this->generate->getGeneratedFilesCount());
     }
+
+    public function testHasGeneratedClass() {
+        $classes = $this->generate->getGeneratedClassFiles();
+        $this->assertArrayHasKey('User',$models = $classes['model']->getGeneratedClassFiles());
+        $this->assertStringContainsString( 'use App\Model\AbstractModel',$models['User']['content']);
+        $this->assertTrue(file_exists("tests/data/".$models['User']['dir'] . "/User.php"));
+    }
+
+    function tearDown(): void {
+       $this->__deleteDir('tests/data');
+    }
+
+    protected function __deleteDir($dir) {
+        if (!is_dir($dir)) {
+            return;
+        }
+    
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (is_dir($dir . "/" . $object)) {
+                    $this->__deleteDir($dir . "/" . $object);
+                } else {
+                    unlink($dir . "/" . $object);
+                }
+            }
+        }
+    
+        rmdir($dir);
+    }    
 }
