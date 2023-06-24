b0776a30deea491c30ee3e6bc38df948dfd1890f
Kendall Arneaud <kendall.arneaud@gmail.com>
2023-06-24 09:51:49 +0000

    refactor class and added save files rouine

--- src/Generate.php
+++ src/Generate.php
@@ -2,18 +2,21 @@
 
 namespace SwaggerGen;
 
+use RuntimeException;
+use Nette\FileNotFoundException;
 use Symfony\Component\Yaml\Yaml;
 use SwaggerGen\Generator\GenerateModels;
 use SwaggerGen\Generator\GenerateRequests;
 #use SwaggerGen\Generator\GeneratorResponse;
 use SwaggerGen\Generator\GeneratorInterface;
 
-class Generate
+final class Generate
 {
     public $saved_models = 0;
     public $saved_requests = 0;
 
     private $dir_path;
+    private $namespace;
     private $generateDefaults = [
         'model' => true,
         'request' => true,
@@ -40,10 +43,11 @@ class Generate
         ?string $generate_class_path = null,
         ?array $additional_classes = null
     ) {
-	    $yaml_options = preg_match("/\.json$/", $yaml_file)? (array) json_decode(file_get_contents($yaml_file), true) : Yaml::parse(file_get_contents($yaml_file),true);
-        $this->base_path = $dir;
+	    $this->base_path = $dir;
+        $this->namespace = $namespace;
         $this->generateOptions = is_null($additional_classes)? $this->generateDefaults : array_filter(array_merge($this->generateDefaults, $additional_classes));
 
+        $yaml_options = preg_match("/\.json$/", $yaml_file)? (array) json_decode(file_get_contents($yaml_file), true) : Yaml::parse(file_get_contents($yaml_file),true);
         $generate_class_path = $generate_class_path ?? 'SwaggerGen\\Generator';
 
 	    $this->generate( $namespace, $generate_class_path, $yaml_options, $more_specificity );
@@ -67,6 +71,8 @@ class Generate
                 $this->addClass($option, $generator);
             }
         }
+
+        $this->saveClasses();
     }
 
     /**
@@ -77,45 +83,6 @@ class Generate
     {
         $this->class_files[$type] = $generator;
     }
-
-    /**
-     * Generates model classes based on the provided YAML file
-     *
-     * @param array  $yamlData
-     * @param string $namespace
-     * @param bool $specificty
-     */
-    public function generateModelClasses(string $namespace, $yaml_data, $specificity = false)
-    {
-        $this->addClass('model' , new GenerateModels($namespace, $yaml_data, $specificity ));
-    }
-
-    /**
-     * Generates request classes based on the provided YAML file
-     *
-     * @param array  $yamlData
-     * @param string $namespace
-     * @param string $dir
-     * @param bool   $specificity
-     */
-    public function generateRequestClasses(string $namespace,array $yaml_data, bool $specificity = false)
-    {
-        $this->addClass('request' , new GenerateModels($namespace, $yaml_data, $specificity ));
-    }
-
-    /**
-     * Generates response classes based on the provided YAML file
-     *
-     * @param array  $yamlData
-     * @param string $namespace
-     * @param bool $specificity
-     */
-    public function generateResponseClasses(string $namespace, array $yaml_data, bool $specificity = false)
-    {
-        $generator = new GenerateResponses($namespace, $yaml_data, $specificity);
-        $this->addClass('response',$generator);
-    }
-
     function getGeneratedClassFiles(): array {
         return $this->class_files;
     }
@@ -129,4 +96,40 @@ class Generate
 
         return $totalCount;
     }
+
+	/**
+	 * Saves generated classes down as PHP files
+     * 
+	 * @throws RuntimeException
+	 * @throws FileNotFoundException
+	 */
+	protected function saveClasses(): void{
+		if (empty($this->class_files)){
+			throw new RuntimeException('No classes were created, try running the generate() method first');
+		}
+
+		foreach ($this->class_files as $type => $generator) {
+            $class_files = $generator->getGeneratedClassFiles();
+            foreach($class_files as $model_name => $class)
+			{
+                $dir = $this->checkDir(sprintf("%s%s%s",$this->base_path,DIRECTORY_SEPARATOR,$class['dir']));
+                file_put_contents(sprintf("%s%s%s.php",$dir,DIRECTORY_SEPARATOR,$model_name), $class['content']);
+            }
+        }
+	}
+	/**
+	 * @param string $dir
+	 * @return string
+	 * @throws FileNotFoundException
+	 */
+	private function checkDir(string $dir): string{
+		if (!file_exists($dir)){
+			mkdir($dir, 0775, true);
+		}
+		if (!file_exists($dir)){
+			throw new FileNotFoundException("The directory $dir did not exist and could not be created");
+		}
+
+		return $dir;
+	}
 }
